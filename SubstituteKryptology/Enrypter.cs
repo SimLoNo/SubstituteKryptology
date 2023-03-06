799a6b5d448029d197791c24efaa2e029e4cc5f9
Simon Nørregaard <Noerregaard.simon@gmail.com>
2023-03-06 10:25:48 +0100

    Add project files.

---
+++ SubstituteKryptology/Enrypter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstituteKryptology
+{
+    public class Enrypter
+    {
+
+        public string Encrypt(string text)
+        {
+            string returnText = "";
+            char newSign;
+            foreach (char sign in text)
+            {
+                int unicode = sign;
+                if (unicode < 127)
+                {
+                    unicode++;
+                }
+                else if (unicode == 127)
+                {
+                    unicode = 1;
+                }
+                else { continue; }
+
+                newSign = System.Convert.ToChar(unicode);
+                returnText+= newSign;
+            }
+            return returnText;
+        }
+
+        public string Decrypt(string text) 
+        {
+            string returnText = "";
+            char newSign;
+            foreach (char sign in text)
+            {
+                int unicode = sign;
+                if (unicode >1 && unicode < 128)
+                {
+                    unicode--;
+                }
+                else if (unicode == 1)
+                {
+                    unicode = 127;
+                }
+                else { continue; }
+
+                newSign = System.Convert.ToChar(unicode);
+                returnText += newSign;
+
+            }
+            return returnText;
+        }
+    }
+}
---
+++ SubstituteKryptology/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Indsæt text.");
+string text = Console.ReadLine();
+SubstituteKryptology.Enrypter encrypter = new SubstituteKryptology.Enrypter();
+string encryptedText = encrypter.Encrypt(text);
+Console.WriteLine(encryptedText);
+string decryptedText = encrypter.Decrypt(encryptedText);
+Console.WriteLine("Original text:");
+Console.WriteLine(text);
+
+Console.WriteLine("Decrypted text:");
+Console.WriteLine(decryptedText);
+
+if (decryptedText == text)
+{
+    Console.WriteLine("Success!");
+}
+else
+{
+    Console.WriteLine("Error!");
+}
+Console.WriteLine("Press enter to close.");
+Console.ReadLine();
